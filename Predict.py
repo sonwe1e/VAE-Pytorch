cf95f7dbdc8fbb80081b862b41cf701e85ecda56
sonwe1e <sonwe1e@outlook.com>
2023-03-27 13:27:49 +0800

    更换保存文件夹；重新定义隐变量；修改测试的显卡；添加model.eval()

--- Predict.py
+++ Predict.py
@@ -5,9 +5,9 @@ from MNISTModel import CVAE
 from FaceModel import FaceVAE
 from utils import label2onehot
 
-model = FaceVAE().cuda(2)
+model = FaceVAE().cuda(3)
 
-old_state_dict = torch.load('checkpoints/cvae-epoch=40-val_loss=224579.20.ckpt')['state_dict']
+old_state_dict = torch.load('checkpoints/cvae-epoch=22-val_loss=399307.66.ckpt', map_location='cuda:3')['state_dict']
 new_state_dict = OrderedDict()
 
 for k, v in old_state_dict.items():
@@ -15,9 +15,10 @@ for k, v in old_state_dict.items():
     new_state_dict[name] = v
     
 model.load_state_dict(new_state_dict)
+model.eval()
 
-test_z = torch.randn((10, 120, 1, 1), device='cuda:2')
-test_class = torch.ones((10, 40, 1, 1), device='cuda:2', dtype=torch.int64) - 2
+test_z = torch.randn((10, 120, 1, 1), device='cuda:3')
+test_class = torch.sign(torch.randn((10, 40, 1, 1), device='cuda:3')-1)
 test_class[:, 20, :, :] = 1
 # test_class = label2onehot(test_class, 10)
 test_z = torch.cat([test_z, test_class], dim=1)
@@ -26,4 +27,4 @@ output = model.decoder(test_z)
 output = output.permute(0, 2, 3, 1).cpu().detach().numpy()
 print(output.shape)
 for i in range(10):
-    cv2.imwrite(f'8/{i}.png', 255*output[i])
+    cv2.imwrite(f'Example/Prediction/{i}.png', 255*output[i])
