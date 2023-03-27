4ba13ed2a9834a17b1101ecade0369d3fe4e6168
sonwe1e <sonwe1e@outlook.com>
2023-03-27 13:29:48 +0800

    Init

--- Train.py
+++ Train.py
@@ -21,40 +21,36 @@ class PL(pl.LightningModule):
         x, y = batch
         output = self.model(x, y)
         x_hat, x, mu, logvar = output
-<<<<<<< HEAD
-        loss = F.mse_loss(x_hat, x, reduction='sum') -0.1 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-=======
-        loss = F.mse_loss(x_hat, x, reduction='sum') -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
->>>>>>> 157ad8c368c323894a7771af5ec784761f2f4e9e
-        self.log('train_loss', loss)
+        y_mu = self.model.mu_conv(self.model.encoder(x)[-1])
+        y_logvar = self.model.logvar_conv(self.model.encoder(x)[-1])
+        loss = F.mse_loss(x_hat, x, reduction='sum') -5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp()) - 5 * torch.sum(1 + y_logvar - y_mu.pow(2) - y_logvar.exp())
+        self.log('train_loss', loss, prog_bar=True)
+        self.log('train_mu', torch.mean(mu), prog_bar=True)
+        self.log('train_logvar', torch.mean(logvar), prog_bar=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
         output = self.model(x, y)
         x_hat, x, mu, logvar = output
-<<<<<<< HEAD
-        loss = F.mse_loss(x_hat, x, reduction='sum') -0.1 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-=======
-        loss = F.mse_loss(x_hat, x, reduction='sum') -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
->>>>>>> 157ad8c368c323894a7771af5ec784761f2f4e9e
+        loss = F.mse_loss(x_hat, x, reduction='sum') -5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
         # class_feature = torch.arange(10).reshape(10, 1).cuda()
         # class_feature = label2onehot(class_feature, 10)
-        class_feature = torch.sign(torch.randn((10, 40, 1, 1), device=self.device)-0.7)
+        class_feature = torch.sign(torch.randn((10, 40, 1, 1), device=self.device)-0.8)
         test_z = torch.randn((10, 120, 1, 1), device=self.device)
         test_z = torch.cat([test_z, class_feature], dim=1)
         test_z = self.model.decoder(test_z)
         for i in range(10):
-            cv2.imwrite(f'FromX/{i}.png', 255*x_hat[i].permute(1, 2, 0).float().cpu().detach().numpy())
+            cv2.imwrite(f'./Example/FromX/{i}.png', 255*x_hat[i].permute(1, 2, 0).float().cpu().detach().numpy())
         for i in range(10):
-            cv2.imwrite(f'FromZ/{i}.png', 255*test_z[i].permute(1, 2, 0).float().cpu().detach().numpy())
+            cv2.imwrite(f'./Example/FromZ/{i}.png', 255*test_z[i].permute(1, 2, 0).float().cpu().detach().numpy())
         self.log('val_loss', loss)
-        self.log('mu', torch.mean(mu), prog_bar=True)
-        self.log('logvar', torch.mean(logvar), prog_bar=True)
+        self.log('valid_mu', torch.mean(mu), prog_bar=True)
+        self.log('valid_logvar', torch.mean(logvar), prog_bar=True)
         return loss
 
     def configure_optimizers(self):
-        return torch.optim.AdamW(self.parameters(), lr=2e-4)
+        return torch.optim.AdamW(self.parameters(), lr=1e-5)
 
 if __name__ == '__main__':
     print('Getting data...')
@@ -69,6 +65,8 @@ if __name__ == '__main__':
         mode='min',
     )
     trainer = pl.Trainer(max_epochs=1000, callbacks=[checkpoint_callback],
-                         accelerator='gpu', devices=[3])
+                         accelerator='auto', devices=[0])
     print('Start training...')
-    trainer.fit(model, train_loader, val_loader)
+    trainer.fit(model, train_loader, val_loader,
+                # ckpt_path='checkpoints/cvae-epoch=91-val_loss=359209.84.ckpt'
+                )
